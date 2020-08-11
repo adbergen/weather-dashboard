3dec4af37eb59325c1173573bcea702c3bc677dd
Anthony Bergen <50747592+adbergen@users.noreply.github.com>
2020-08-11 09:55:25 -0400

    added function to verify city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
 
+  // stored searches
   var citySearched = "";
 
+  // variables for pulled data
   var userCity = $("user-city");
   var searchCity = $("search-city");
   var clearHistory = $("clear-history");
-  var currentCity = $("")
+  var currentCity = $("current-city")
+  var currentTemp = $("current-temp")
+  var currentHumid = $("current-humid")
+  var currentWind = $("current-wind")
+  var currentUV = $("current-uv")
+  var cities = [];
 
-
+  // verify city
+  function find(c) {
+    for (var i = 0; i < cities.length; i++) {
+      if (c.toUpperCase() === cities[i]) {
+        return -1;
+      }
+    }
+    return 1;
+  }
 
   // search click function
   $("#searchCity").on("click", function (event) {
